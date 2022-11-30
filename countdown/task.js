3f6df49cb2a6175b0315e806ebe3da10e602e5e6
Anatoliy Oleynikov <oleinikovav.ao@gmail.com>
2022-11-30 18:54:21 +0300

    34

--- countdown/task.js
+++ countdown/task.js
@@ -1,27 +1,17 @@
-// при старте счетчика times() в первую секунду возникает 1,затем обратный отсчет нормальный, для очистки сделал ф-ю с clearInterval.
-
-/*function f() {
-  // функция для очистки clearInterval, вместо 1 будет underfind, но это неправильно...
-  clearInterval(current.innerText);
-  setInterval(times, 1000);
-}*/
-
 const current = document.getElementById("timer");
 
 let sec = new Date();
 sec = sec.getSeconds();
-console.log(sec);
+current.textContent = sec;
 
-current.innerText = setInterval(times, 1000);
-setTimeout(() => {clearInterval(current.innerText); alert("stop")}, 3000);
+let t = setInterval(times, 1000);
 
 function times() {
   if (sec > 0) {
     sec--;
-    current.innerText = sec;
+    current.textContent = sec;
   } else if (sec == 0) {
     alert("Вы победили в конкурсе!");
-    
+    clearInterval(t);
   }
-  console.log(sec);
 }
